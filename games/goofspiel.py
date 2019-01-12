6961bebe3c9c221addd1d7d66051af3b08e7dcf0
Tommaso Bianchi <tommaso4.bianchi@mail.polimi.it>
2019-01-12 15:47:35 +0100

    Goofspiel infoset implemented plus some adjustments for tree compression

--- games/goofspiel.py
+++ games/goofspiel.py
@@ -1,4 +1,5 @@
 from data_structures.trees import Tree, Node, ChanceNode
+from functools import reduce
 
 def build_goofspiel_tree(num_players, rank):
     """
@@ -12,11 +13,24 @@ def build_goofspiel_tree(num_players, rank):
 
     hands = all_permutations(list(range(1, rank+1)))
     hand_probability = 1 / len(hands)
+    all_nodes = []
 
     for hand in hands:
         n = tree.addNode(0, parent = root, probability = hand_probability, actionName = str(hand))
-        build_goofspiel_hand_tree(hand, [list(range(1, rank+1)) for p in range(num_players)],
-                                  [[] for p in range(num_players)], 0, 0, n, tree)
+        n.known_information = (0, hand[:1], [[] for p in range(num_players)])
+        all_nodes.append(n)
+        all_nodes += build_goofspiel_hand_tree(hand, [list(range(1, rank+1)) for p in range(num_players)],
+                                  [[] for p in range(num_players)], 0, 1, n, tree)
+
+    # Merge nodes into infosets based on the available information at each node
+    for i1 in range(len(all_nodes)):
+        for i2 in range(i1+1, len(all_nodes)):
+            if(all_nodes[i1].player != all_nodes[i2].player or all_nodes[i1].isLeaf() or all_nodes[i2].isLeaf()):
+                continue
+            if(all_nodes[i1].known_information == all_nodes[i2].known_information):
+                iset_id = min(all_nodes[i1].information_set, all_nodes[i2].information_set)
+                all_nodes[i1].information_set = iset_id
+                all_nodes[i2].information_set = iset_id
             
     return tree
 
@@ -25,10 +39,6 @@ def build_goofspiel_hand_tree(hand, remaining_cards, played_cards, current_round
     Recursively build the subtree for the Kuhn game where the hand is fixed.
     """
 
-    if(len(remaining_cards[current_player]) == 0):
-        tree.addLeaf(parent = current_node, utility = goofspiel_utility(hand, played_cards))
-        return
-
     num_players = tree.numOfPlayers
     if(current_player == num_players-1):
         next_player = 0
@@ -36,18 +46,37 @@ def build_goofspiel_hand_tree(hand, remaining_cards, played_cards, current_round
     else:
         next_player = current_player + 1
         next_round = current_round
-
-    information_set = -1
+        
     current_player_cards = remaining_cards[current_player].copy()
 
+    # Create a leaf as a children of the last effective decision node (there is no decision for players that
+    # have only their last card in hand)
+    if(len(remaining_cards[current_player]) == 2 and len(remaining_cards[next_player]) == 1):
+        for card in current_player_cards:
+            actionName = "p" + str(current_node.player) + "c" + str(card)
+            remaining_cards[current_player].remove(card)
+            played_cards[current_player].append(card)
+            final_played_cards = [played_cards[i] + remaining_cards[i] for i in range(len(played_cards))]
+            l = tree.addLeaf(parent = current_node, utility = goofspiel_utility(hand, final_played_cards))
+            remaining_cards[current_player].append(card)
+            played_cards[current_player].remove(card)
+        return [l]
+
+    information_set = -1
+    nodes = []
+
     for card in current_player_cards:
-        actionName = "p" + str(current_player) + "c" + str(card)
+        actionName = "p" + str(current_node.player) + "c" + str(card)
         n = tree.addNode(current_player, information_set, parent = current_node, actionName = actionName)
+        n.known_information = (current_round, hand[:current_round+1], [c[:current_round] for c in played_cards])
+        nodes.append(n)
         remaining_cards[current_player].remove(card)
         played_cards[current_player].append(card)
-        build_goofspiel_hand_tree(hand, remaining_cards, played_cards, next_round, next_player, n, tree)
+        nodes += build_goofspiel_hand_tree(hand, remaining_cards, played_cards, next_round, next_player, n, tree)
         remaining_cards[current_player].append(card)
         played_cards[current_player].remove(card)
+
+    return nodes
 
 def build_all_possible_hands(num_players, ranks):
     """
