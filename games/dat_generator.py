e122a69de211e89f374d78eb106eb51e84e7a115
Andrea Celli <amcelli@gmail.com>
2019-01-15 14:18:11 +0100

    removed redundant code

--- games/dat_generator.py
+++ games/dat_generator.py
@@ -2,34 +2,6 @@ import argparse
 from utilities import serialization
 import goofspiel, kuhn, leduc
 
-#     parser = argparse.ArgumentParser(description='input parser')
-#     parser.add_argument('game_instance', type=str, help='game instance to '
-#                                                         'be solved: '
-#                                                         'kuhn3_3cards, '
-#                                                         'kuhn3_4cards')
-#     parser.add_argument('solver_type', type=str, help='alg to be employed: '
-#                                                       'no_discard')
-#     parser.add_argument('-ps', '--pool_size', type=int, default=50, \
-#                         help='number of '
-#                              'sigmas '
-#                              'considered '
-#                              'at the same '
-#                              'time in the '
-#                              'auxiliry '
-#                              'tree')
-#     parser.add_argument('-n_iter', '--n_iterations_cfr', type=int, default=1000,
-#                         help='number of iterations for each regret minimizer')
-#     parser.add_argument('-init', '--init_type', type=str, default='random',
-#                         help='type of pool init: random')
-#     parser.add_argument('-n_iter_fictitious',
-#                         '--n_iterations_fictitious_play', type=int,
-#                         default=1000)
-#
-#     args = parser.parse_args()
-#
-#     game_instance = args.game_instance
-# solver_type = args.solver_type
-
 # TODO LEDUC
 GAMES = ['kuhn', 'goofspiel']
 
